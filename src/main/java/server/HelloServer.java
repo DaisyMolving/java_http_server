688878a34963d368eda449a5b5bd27deb05549e7
DaisyMolving <daisymolving@gmail.com>
2017-02-22 15:48:46 +0000

    simple get

---
+++ src/main/java/server/HelloServer.java
@@ -0,0 +1,65 @@
+package server;
+
+import java.io.*;
+import java.net.*;
+import java.util.HashMap;
+
+public class HelloServer {
+
+    public ServerSocket serverSocket;
+    public Socket clientConnection;
+
+
+    public void start(String[] args) throws IOException {
+        int portNumber= Integer.parseInt(args[1]);
+        bindServerSocketToPort(portNumber);
+        for(;;) {
+            acceptConnectionFromClient();
+
+            BufferedReader input = new BufferedReader(new InputStreamReader(clientConnection.getInputStream()));
+            PrintStream output = new PrintStream(clientConnection.getOutputStream());
+            String request = input.readLine();
+            System.out.println(request);
+
+            HashMap<String, String> requestDefinition = define(request);
+            String methodVerb = requestDefinition.get("methodVerb");
+            String path = requestDefinition.get("path");
+            String protocolVersion = requestDefinition.get("protocolVersion");
+
+            if (methodVerb.equals("GET")) {
+                System.out.println("get request");
+                output.println(protocolVersion + " " + "200 OK");
+            }
+
+            input.close();
+            output.close();
+            clientConnection.close();
+            System.out.println("I am looping to find new requests");
+        }
+    }
+
+    public void bindServerSocketToPort(int portNumber) {
+        try {
+            serverSocket = new ServerSocket(portNumber);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void acceptConnectionFromClient() {
+        try {
+            clientConnection = serverSocket.accept();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private HashMap<String, String> define(String request) {
+        String[] splitRequest = request.split("\\s+");
+        HashMap<String, String> requestDefinition = new HashMap<>();
+        requestDefinition.put("methodVerb", splitRequest[0]);
+        requestDefinition.put("path", splitRequest[1]);
+        requestDefinition.put("protocolVersion", splitRequest[2]);
+        return requestDefinition;
+    }
+}
---
+++ src/main/java/server/Main.java
@@ -0,0 +1,9 @@
+package server;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        HelloServer server = new HelloServer();
+        server.start(args);
+    }
+}
